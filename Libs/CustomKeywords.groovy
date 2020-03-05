2ae6a8338419b00f6291d895d024979860a0041e
Ushani De Mel <ushanidemel1@gmail.com>
2020-03-05 11:23:14 +0530

    first commit

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,13 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import groovy.lang.Closure
+
+
+def static "com.katalon.jmeter.JMeterKeyword.createSampler"(
+    	Closure testAction	) {
+    (new com.katalon.jmeter.JMeterKeyword()).createSampler(
+        	testAction)
+}
---
+++ Libs/TempTestSuite1582266351150.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/MTN Development/MTN Subscription')
+
+suiteProperties.put('name', 'MTN Subscription')
+
+suiteProperties.put('description', '')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("/home/ushani/gitRepos/Orange_Product_Development/PDORNG_API_Development/Reports/20200221_115551/MTN Development/MTN Subscription/20200221_115551/execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/MTN Development/MTN Subscription', suiteProperties, new File("/home/ushani/gitRepos/Orange_Product_Development/PDORNG_API_Development/Reports/20200221_115551/MTN Development/MTN Subscription/20200221_115551/testCaseBinding"))
