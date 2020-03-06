7756766ea87d2690b15fc263033d430bbc6ecf58
Ushani De Mel <ushanidemel1@gmail.com>
2020-03-06 14:33:25 +0530

    My third commit remove video

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -3,11 +3,3 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
-import groovy.lang.Closure
-
-
-def static "com.katalon.jmeter.JMeterKeyword.createSampler"(
-    	Closure testAction	) {
-    (new com.katalon.jmeter.JMeterKeyword()).createSampler(
-        	testAction)
-}
